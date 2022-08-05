8cc763fc84bc280d1212be12adf3c2500f5240ba
Da_Loy <loyisondlovu6@gmail.com>
2022-08-05 11:29:55 +0200

    I created a function to get the computer's selection in the game.
    I created a function to prompt the player to make a choice and store
    choice.
    
    i created a single round function but it has no code at the moment

--- java.js
+++ java.js
@@ -1,9 +1,21 @@
-const options = ["Rock","Paper","Scissors"]
+const options = ["Rock","Paper","Scissors"] //Options from which the getComputerChoice function selects from.
 
-function  getComputerChoice () {
+computerSelection = getComputerChoice () //sets computerSelectio to result of getComputerChoice
+
+playerSelection = getPlayerChoice() //sets playerSelection to result of getPlayerChoice
+
+function  getComputerChoice () { 
     let choice = options[Math.floor(Math.random() * options.length)]
-    console.log(choice);
+    return choice // creates a variable called choice which holds the result from the options 
 }
 
-getComputerChoice ()
+function getPlayerChoice () { // gets the player's choice 
+    return playerChoice = prompt("What do you choose sir?") //prompts the player for a choice and stores the answer in playerChoice
+    
+}
+
+function singleRound (playerSelection, computerSelection) {
+}
+
+console.log(playerSelection)
 
