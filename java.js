b5b011bd8ac1e950a63716e9c33d31ad7abd180a
Da_Loy <loyisondlovu6@gmail.com>
2022-09-06 18:50:15 +0200

    added a rock, paper and scissors buttons on Html. created rockBtn, paperBtn and scissorBtn nodes and linked them to the RPS buttons in index.html

--- java.js
+++ java.js
@@ -1,25 +1,3 @@
-/*const options = ["Rock","Paper","Scissors"] //Options from which the getComputerChoice function selects from.
-
-computerSelection = getComputerChoice () //sets computerSelectio to result of getComputerChoice
-
-playerSelection = getPlayerChoice() //sets playerSelection to result of getPlayerChoice
-
-function  getComputerChoice () { 
-    let choice = options[Math.floor(Math.random() * options.length)]
-    return choice // creates a variable called choice which holds the result from the options 
-}
-
-function getPlayerChoice () { // gets the player's choice 
-    return playerChoice = prompt("What do you choose sir?") //prompts the player for a choice and stores the answer in playerChoice
-    
-}
-
-function singleRound (playerSelection, computerSelection) {
-}
-
-console.log(playerSelection)
-*/    
-    
 const options = ["rock","paper","scissors"] //Options from which the getComputerChoice function selects from.
 
 computerSelection = getComputerChoice () //sets computerSelectio to result of getComputerChoice
@@ -60,4 +38,7 @@ function singleRound (choice, playerChoice) {
 
 singleRound(computerSelection, playerSelection)
 
-    
+const rockBtn = document.querySelector('#rock');
+const paperBtn  = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
+
